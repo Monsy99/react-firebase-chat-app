850fc42f7de9b2f4f1cf95d99cf6bc3851543128
Szymon Marczuk <szymon.marczukk@gmail.com>
2020-11-19 12:51:56 +0100

    added GlobalStyle to app

---
+++ src/globalStyle.js
@@ -0,0 +1,12 @@
+import {createGlobalStyle} from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    body {
+    margin: 0;
+    color: white;
+    background-color: black;
+    font-family: 'Roboto', sans-serif;
+    }
+`;
+
+export default GlobalStyle;
