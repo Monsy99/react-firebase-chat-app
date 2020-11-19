993aa7b43efc24fa1ac636e557bd9a9e2c84ce3c
Szymon Marczuk <szymon.marczukk@gmail.com>
2020-11-19 12:42:18 +0100

    added default ChatRoom logic without styling

---
+++ src/components/ChatRoom/index.js
@@ -0,0 +1,47 @@
+import React, { useRef, useState } from "react";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+
+
+export default ({ firebase, firestore, auth }) => {
+    const messagesRef = firestore.collection('messages');
+    const getMessagesByTime = messagesRef.orderBy('createdAt').limit(25);
+    const [messages] = useCollectionData(getMessagesByTime);
+
+    const [input,setInput] = useState('');
+    const onInputChange = (e) => {
+        setInput(e.target.value);
+    }
+    const onFormSubmit = async (e) => {
+        e.preventDefault();
+        const { uid, photoURL } = auth.currentUser;
+
+        await messagesRef.add(
+            {
+                text: input,
+                createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+                uid,
+                photoURL,
+            }
+        )
+        setInput('');
+        dummy.current.scrollIntoView({ behavior: 'smooth' })
+    }
+    const dummy = useRef();
+
+    return (
+        <div>
+            <ul>
+                { messages && messages.map( message => <li className={message.id}>
+                <img src={message.photoURL}></img>
+                {message.text}
+                </li>) }
+                <div ref={dummy}></div>
+            </ul>
+            <form onSubmit={onFormSubmit}>
+                <input value={input} onChange={onInputChange}>
+                </input>
+                <button>Send</button>
+            </form>
+        </div>
+    );
+}
