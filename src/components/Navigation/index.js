a30d2a2aa8bc8dbf87986c2e457c4f16b020a4b7
Szymon Marczuk <56971694+Monsy99@users.noreply.github.com>
2021-01-02 20:48:26 +0100

    changed Navigation to be getting only firebase as input and formatted its styling

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { NavBar, Logo } from "./styled.js";
 import SignOut from "../SignOut";
 import SignIn from "../SignIn";
 
-const Navigation = ({auth}) => {
-    return(
-        <NavBar>
-            <Logo>Chat App</Logo>
-            {auth.currentUser ? <SignOut auth={auth}/> : <SignIn auth={auth}/>}
-        </NavBar>
-    );
-}
+const Navigation = ({ firebase }) => {
+  const auth = firebase.auth();
+  useEffect(() => {}, [auth.currentUser]);
+  console.log(auth.currentUser);
+  return (
+    <NavBar>
+      <Logo>Chat App</Logo>
+      {auth.currentUser ? <SignOut auth={auth} /> : <SignIn auth={auth} />}
+    </NavBar>
+  );
+};
 
 export default Navigation;
--- src/components/Navigation/styled.js
+++ src/components/Navigation/styled.js
@@ -1,20 +1,21 @@
 import styled from "styled-components";
 
 const NavBar = styled.nav`
-    width:100%;
-    max-width:1200px;
-    display: flex;
-    align-items:center;
-    justify-content:space-between;
-    margin: 0 auto;
+  width: 100%;
+  max-width: 1200px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: 0 auto;
+  box-shadow: 0 0px 5px rgba(255, 255, 255, 0.6);
 `;
 const Logo = styled.h1`
-margin: 0 12px;
-font-family: 'Sansita Swashed', cursive;
-font-size: 28px;
-font-weight:bold;
-color:#eee;
-padding: 12px;
+  margin: 0 12px;
+  font-family: "Sansita Swashed", cursive;
+  font-size: 28px;
+  font-weight: bold;
+  color: #eee;
+  padding: 12px;
 `;
 
-export {NavBar, Logo}
+export { NavBar, Logo };
