e72a71898c9a230d7620cac53b19a812e292ef49
Szymon Marczuk <56971694+Monsy99@users.noreply.github.com>
2021-01-05 11:28:22 +0100

    removed user console logs

--- src/components/ChatRoom/index.js
+++ src/components/ChatRoom/index.js
@@ -14,9 +14,8 @@ import {
 import { nanoid } from "nanoid";
 import { useParams } from "react-router-dom";
 
-const ChatRoom = ({ firebase }) => {
+const ChatRoom = ({ firebase, user }) => {
   const firestore = firebase.firestore();
-  const auth = firebase.auth();
   const { roomRef } = useParams();
   const chatroomsRef = firestore.collection("chatrooms");
   const messagesRef = chatroomsRef.doc(roomRef).collection("messages");
@@ -26,9 +25,7 @@ const ChatRoom = ({ firebase }) => {
   const currentRoom = chatrooms
     ? chatrooms.find((chatroom) => roomRef === chatroom.ref)
     : null;
-
-  // ----------------------------------------------------
-
+  const currentUserId = user ? user.uid : null;
   const [input, setInput] = useState("");
   const onInputChange = (e) => {
     setInput(e.target.value);
@@ -40,7 +37,7 @@ const ChatRoom = ({ firebase }) => {
   }, [messages]);
   const onFormSubmit = async (e) => {
     e.preventDefault();
-    const { uid, photoURL } = auth.currentUser;
+    const { uid, photoURL } = user;
     if (input.trim() !== "") {
       await messagesRef.add({
         text: input,
@@ -55,48 +52,51 @@ const ChatRoom = ({ firebase }) => {
     }
   };
   const dummy = useRef();
-  const currentUserId = auth.currentUser ? auth.currentUser.uid : null;
-  return (
-    auth && (
-      <RelativeDiv>
-        <h1>Room: {currentRoom ? currentRoom.title : ""}</h1>
-        <MessagesList>
-          {messages &&
-            messages.map((message) => (
-              <Message
-                isAuthor={message.uid === currentUserId}
-                key={nanoid()}
-                className={message.id}
-              >
-                <AuthorAvatar
-                  alt={"avatar"}
-                  src={message.photoURL}
-                ></AuthorAvatar>
-                <MessageText>{message.text}</MessageText>
-                <MessageTime>
-                  {message.createdAt
-                    ? `${new Date(
-                        message.createdAt.seconds * 1000
-                      ).toLocaleTimeString()}
+
+  return user ? (
+    <RelativeDiv>
+      <h1>Room: {currentRoom ? currentRoom.title : ""}</h1>
+      <MessagesList>
+        {messages &&
+          messages.map((message) => (
+            <Message
+              isAuthor={message.uid === currentUserId}
+              key={nanoid()}
+              className={message.id}
+            >
+              <AuthorAvatar
+                alt={"avatar"}
+                src={message.photoURL}
+              ></AuthorAvatar>
+              <MessageText>{message.text}</MessageText>
+              <MessageTime>
+                {message.createdAt
+                  ? `${new Date(
+                      message.createdAt.seconds * 1000
+                    ).toLocaleTimeString()}
                      ${new Date(
                        message.createdAt.seconds * 1000
                      ).toLocaleDateString()}`
-                    : ``}
-                </MessageTime>
-              </Message>
-            ))}
-          <div ref={dummy}></div>
-        </MessagesList>
-        <MessageForm onSubmit={onFormSubmit}>
-          <FormInput
-            placeholder={"Write a message here"}
-            value={input}
-            onChange={onInputChange}
-          />
-          <FormSubmit>⬆️</FormSubmit>
-        </MessageForm>
-      </RelativeDiv>
-    )
+                  : ``}
+              </MessageTime>
+            </Message>
+          ))}
+        <div ref={dummy}></div>
+      </MessagesList>
+      <MessageForm onSubmit={onFormSubmit}>
+        <FormInput
+          placeholder={"Write a message here"}
+          value={input}
+          onChange={onInputChange}
+        />
+        <FormSubmit>⬆️</FormSubmit>
+      </MessageForm>
+    </RelativeDiv>
+  ) : (
+    <h3>
+      Hi ! <br /> Looks like you are not logged in <br /> Please log in to
+      display the messages
+    </h3>
   );
 };
 export default ChatRoom;
