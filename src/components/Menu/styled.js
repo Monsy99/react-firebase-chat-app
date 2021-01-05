8b9f8190402b4be9a834e5722a7e84daa30eecbd
Szymon Marczuk <56971694+Monsy99@users.noreply.github.com>
2021-01-05 11:05:55 +0100

    added a title component and changed Room component a bit

--- src/components/Menu/styled.js
+++ src/components/Menu/styled.js
@@ -1,18 +1,27 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
-const Container = styled.div`
-  border-right: 1px solid rgba(255, 255, 255, 0.6);
-`;
+const Container = styled.div``;
 
 const StyledLink = styled(Link)`
   text-decoration: none;
   color: #ccc;
   width: 100%;
 `;
+const Title = styled.p`
+  margin: 0;
+  font-family: "Roboto", sans-serif;
+  font-size: 18px;
+  &::before {
+    content: "#";
+    font-size: 20px;
+  }
+`;
 const Room = styled.div`
   width: 100%;
-  padding: 4px 12px;
-  border-bottom: 1px solid #eee;
+  padding: 8px 12px;
+  border: 1px solid #eee;
+  border-radius: 8px;
+  margin: 4px 0;
 `;
 
-export { Container, StyledLink, Room };
+export { Container, StyledLink, Room, Title };
