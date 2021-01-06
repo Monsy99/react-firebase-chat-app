3ca7b9921445a2783a0db56b16e9ef9bd7f73fe3
Szymon Marczuk <56971694+Monsy99@users.noreply.github.com>
2021-01-06 20:59:22 +0100

    added a theme provider to the project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ThemeProvider } from "styled-components";
 import App from "./App";
 import GlobalStyle from "./globalStyle";
+import theme from "./theme";
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyle />
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
---
+++ src/theme.js
@@ -0,0 +1,12 @@
+const theme = {
+  color: {
+    mineSchaft: "#333333",
+    butterflyBush: "#6e5494",
+    wildSand: "#f5f5f5",
+    alabaster: "#fafafa",
+    tiaMaria: "#c9510c",
+    milanoRed: "#bd2c00",
+    mantis: "#6cc644",
+    indigo: "#4078c0",
+  },
+};
