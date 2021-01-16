70ade56b2e3f222fb70581e0c9ec4a67738dac2d
Szymon Marczuk <szymon.marczukk@gmail.com>
2021-01-16 20:50:06 +0100

    added a redirect from login page if user is already set up

--- src/components/LoginPage/index.js
+++ src/components/LoginPage/index.js
@@ -1,7 +1,14 @@
+import { useSelector } from "react-redux";
+import { Redirect } from "react-router-dom";
+import { selectUser } from "../../userSlice";
 import Navigation from "../Navigation";
 import LoginPrompt from "./LoginPropmt";
 
 const LoginPage = ({ auth, firebase }) => {
+  const storeUser = useSelector(selectUser);
+  if (storeUser) {
+    return <Redirect to="/room/general"></Redirect>;
+  }
   return (
     <>
       <Navigation firebase={firebase}></Navigation>
