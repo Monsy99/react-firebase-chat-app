fdcc862984244036b4c080ec2930a9e5d4fc2e16
Szymon Marczuk <szymon.marczukk@gmail.com>
2021-01-16 20:48:31 +0100

    removed unnecesary styled file from Button component and added marginTop and plain optional properties

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,7 +1,35 @@
-import React from "react";
-import StyledButton from "./styled";
+import styled, { css } from "styled-components";
 
-const Button = ({ children, onClick }) => {
-  return <StyledButton onClick={onClick}>{children}</StyledButton>;
-};
+const Button = styled.button`
+  background-color: transparent;
+  color: ${({ theme }) => theme.color.white};
+  font-size: 16px;
+  line-height: 130%;
+  padding: 12px 22px;
+  border: 2px solid ${({ theme }) => theme.color.buttonBorder};
+  border-radius: 15px;
+  cursor: pointer;
+  transition: background-color 0.3s;
+  &:hover {
+    background-color: ${({ theme }) => theme.color.buttonBorder};
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  }
+  ${(props) =>
+    props.marginTop &&
+    css`
+      margin-top: 24px;
+    `}
+  ${(props) =>
+    props.plain &&
+    css`
+      padding: 12px 22px;
+      color: ${({ theme }) => theme.color.white};
+      border: none;
+      background-color: ${({ theme }) => theme.color.charade};
+      &:hover {
+        box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+      }
+    `}
+`;
+
 export default Button;
