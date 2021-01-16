8bd35f8a8479ab831e8f47180dec05beb1cfaffa
Szymon Marczuk <szymon.marczukk@gmail.com>
2021-01-16 20:54:54 +0100

    added a resetUser dispatch on signIn click

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -3,10 +3,14 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 import Button from "../Button";
+import { useDispatch } from "react-redux";
+import { resetUser } from "../../userSlice";
 
 const SignIn = ({ auth }) => {
+  const dispatch = useDispatch();
   const signInWithGoogle = () => {
     const provider = new firebase.auth.GoogleAuthProvider();
+    dispatch(resetUser());
     auth.signInWithPopup(provider);
   };
   return <Button onClick={signInWithGoogle}>Sign in with Google</Button>;
