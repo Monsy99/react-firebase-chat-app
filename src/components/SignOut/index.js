c0180f484b5e5a17a6ab117b83cb68770eb84fa6
Szymon Marczuk <szymon.marczukk@gmail.com>
2021-01-16 20:56:19 +0100

    added a resetUser function that signs out a storeUser after a timeout

--- src/components/SignOut/index.js
+++ src/components/SignOut/index.js
@@ -1,10 +1,26 @@
 import React from "react";
 import "firebase/auth";
 import Button from "../Button";
+import { useDispatch } from "react-redux";
+import { resetUser } from "../../userSlice";
 
 const SignOut = ({ auth }) => {
+  const dispatch = useDispatch();
   return (
-    auth.currentUser && <Button onClick={() => auth.signOut()}>Sign out</Button>
+    auth.currentUser && (
+      <Button
+        onClick={() => {
+          auth.signOut();
+          setTimeout(() => {
+            dispatch(resetUser());
+          }, 1000);
+          // the setTimeout is here so that the signOut can complete before reseting the user
+          // otherwise the google auth is still signed in and triggers setting user
+        }}
+      >
+        Sign out
+      </Button>
+    )
   );
 };
 export default SignOut;
